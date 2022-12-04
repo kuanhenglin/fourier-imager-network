82f345199d595266979ba82282aacfc9b6fde1dc
Kuan Heng (Jordan) Lin <kuanhenglin@outlook.com>
2022-12-04 14:31:57 -0800

    Clean up run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 cp main.py main_run.py  #further (potentially breaking) modifications to main.py will not halt run
 
 #FOURIER RESNET
-python3 main_run.py -t fourier_resnet -l 0.1 -e 0
-python3 main_run.py -t fourier_parallel -l 0.1 -e 0
-python3 main_run.py -t fourier_skip -l 0.1 -e 0
+python3 main_run.py  -t fourier_resnet    -l 0.1  -f 1 1 1      -s 1  -r 3  -e 0
+python3 main_run.py  -t fourier_parallel  -l 0.1  -f 1 1 1      -s 1  -r 3  -e 0
+python3 main_run.py  -t fourier_skip      -l 0.1  -f 1 1 1      -s 1  -r 3  -e 0
 
 #VANILLA RESNET
-python3 main_run.py -t fourier_resnet -l 0.1 -f 0 0 0 -s 0 -e 0  #ResNet-20
-python3 main_run.py -t fourier_resnet -l 0.1 -r 5 -f 0 0 0 0 0 -s 0 -e 0  #ResNet-32
+python3 main_run.py  -t fourier_resnet    -l 0.1  -f 0 0 0      -s 0  -r 3  -e 0   #ResNet-20
+python3 main_run.py  -t fourier_resnet    -l 0.1  -f 0 0 0 0 0  -s 0  -r 5  -e 0   #ResNet-32
